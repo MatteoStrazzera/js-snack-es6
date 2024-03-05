55f6bff897d5df03c5103267f9835a0187d744a8
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-05 16:38:09 +0100

    add people list in snack 4 js

--- snack 4/main.js
+++ snack 4/main.js
@@ -8,3 +8,60 @@ Ogni persona ha un nome, un cognome e un’età.
 Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
 
 */
+
+const peopleList = [
+    {
+        nome: 'Marco',
+        cognome: 'Rossi',
+        eta: 28
+    },
+    {
+        nome: 'Francesca',
+        cognome: 'Bianchi',
+        eta: 15
+    },
+    {
+        nome: 'Luigi',
+        cognome: 'Verdi',
+        eta: 22
+    },
+    {
+        nome: 'Anna',
+        cognome: 'Rizzo',
+        eta: 40
+    },
+    {
+        nome: 'Giuseppe',
+        cognome: 'Ferrari',
+        eta: 19
+    },
+    {
+        nome: 'Elena',
+        cognome: 'Conti',
+        eta: 17
+    },
+    {
+        nome: 'Alessio',
+        cognome: 'Marini',
+        eta: 26
+    },
+    {
+        nome: 'Maria',
+        cognome: 'Galli',
+        eta: 55
+    },
+    {
+        nome: 'Paolo',
+        cognome: 'Moretti',
+        eta: 33
+    },
+    {
+        nome: 'Giulia',
+        cognome: 'Colombo',
+        eta: 16
+    }
+];
+
+console.log(peopleList);
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
